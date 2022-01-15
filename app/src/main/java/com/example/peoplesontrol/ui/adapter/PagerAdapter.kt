42169659b4e29f5e5a80306c384e542688545f8c
Zellka <ilona.zelka@gmail.com>
2022-01-15 16:59:53 +0300

    Add sort appeals

---
+++ app/src/main/java/com/example/peoplesontrol/ui/adapter/PagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.peoplesontrol.ui.adapter
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.peoplesontrol.R
+import com.example.peoplesontrol.ui.view.main.SortAppealFragment
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_number,
+    R.string.tab_name,
+    R.string.tab_rating,
+    R.string.tab_date
+)
+
+class PagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return SortAppealFragment()
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        return 4
+    }
+}
--- app/src/main/java/com/example/peoplesontrol/ui/view/main/AppealsFragment.kt
+++ app/src/main/java/com/example/peoplesontrol/ui/view/main/AppealsFragment.kt
@@ -2,27 +2,14 @@ package com.example.peoplesontrol.ui.view.main
 
 import android.os.Bundle
 import android.view.*
-import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.peoplesontrol.R
-import com.example.peoplesontrol.data.model.Appeal
 import com.example.peoplesontrol.databinding.FragmentAppealsBinding
-import com.example.peoplesontrol.ui.adapter.AppealAdapter
+import com.example.peoplesontrol.ui.adapter.PagerAdapter
 
 class AppealsFragment : Fragment() {
 
     private var _binding: FragmentAppealsBinding? = null
     private val binding get() = _binding!!
-
-    private lateinit var adapter: AppealAdapter
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,99 +19,7 @@ class AppealsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val list = listOf(
-            Appeal(
-                123,
-                "Иванов Иван Иванович",
-                "Авария",
-                "г.Донецк, ул.Артёма, 66",
-                "53",
-                "26",
-                "В обработке",
-                "",
-                "14.01.22",
-                "Большая авария, 2 машины, 1 пострадавший",
-                0,
-                14
-            ),
-            Appeal(
-                124,
-                "Петров Иван Иванович",
-                "Авария",
-                "г.Донецк, ул.Артёма, 66",
-                "53",
-                "26",
-                "В обработке",
-                "",
-                "14.01.22",
-                "Большая авария, 2 машины, 1 пострадавший",
-                0,
-                1
-            ),
-            Appeal(
-                125,
-                "Иванов Василия Иванович",
-                "Авария",
-                "г.Донецк, ул.Артёма, 66",
-                "53",
-                "26",
-                "В обработке",
-                "",
-                "14.01.22",
-                "Большая авария, 2 машины, 1 пострадавший",
-                0,
-                15
-            ),
-            Appeal(
-                126,
-                "Иванов Юрий Иванович",
-                "Авария",
-                "г.Донецк, ул.Артёма, 66",
-                "53",
-                "26",
-                "В обработке",
-                "",
-                "14.01.22",
-                "Большая авария, 2 машины, 1 пострадавший",
-                0,
-                24
-            )
-        )
-
-        binding.rvAppeals.layoutManager = LinearLayoutManager(this.requireContext())
-        adapter = AppealAdapter { appeal: Appeal -> showAppeal(appeal) }
-        adapter.setData(list)
-        binding.rvAppeals.adapter = adapter
-    }
-
-    private fun showAppeal(appeal: Appeal) {
-        findNavController().navigate(R.id.action_appealsFragment_to_appealFragment)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.search_menu, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        if (id == android.R.id.home){
-            findNavController().navigate(R.id.action_appealsFragment_to_categoriesFragment)
-        }
-        if (id == R.id.action_search) {
-            val searchView = item.actionView as SearchView
-            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                override fun onQueryTextSubmit(p0: String?): Boolean {
-                    return true
-                }
-
-                override fun onQueryTextChange(query: String?): Boolean {
-                    adapter.filter.filter(query)
-                    return true
-                }
-            })
-        }
-        return super.onOptionsItemSelected(item)
+        binding.viewPager.adapter = PagerAdapter(this.requireContext(), childFragmentManager)
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
     }
 }
