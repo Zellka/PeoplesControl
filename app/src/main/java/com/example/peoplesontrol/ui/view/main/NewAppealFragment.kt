cf43c9e21c7b4ca8fc06e5cfe0c89820a1aabcf2
Zellka <ilona.zelka@gmail.com>
2022-01-15 19:56:34 +0300

    Update NewAppealFragment.kt

--- app/src/main/java/com/example/peoplesontrol/ui/view/main/NewAppealFragment.kt
+++ app/src/main/java/com/example/peoplesontrol/ui/view/main/NewAppealFragment.kt
@@ -3,28 +3,36 @@ package com.example.peoplesontrol.ui.view.main
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.peoplesontrol.R
+import com.example.peoplesontrol.databinding.FragmentNewAppealBinding
+import android.widget.ArrayAdapter
 
 class NewAppealFragment : Fragment() {
+
+    private var _binding: FragmentNewAppealBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_new_appeal, container, false)
+    ): View {
+        _binding = FragmentNewAppealBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                findNavController().navigate(R.id.action_appealFragment_to_appealsFragment)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val categories =
+            arrayOf(
+                "Нарушение ПДД",
+                "Скопление животных",
+                "Нарушение КЗОТ",
+                "Городское пространство"
+            )
+        val adapter: ArrayAdapter<String> =
+            ArrayAdapter(this.requireContext(), android.R.layout.simple_spinner_item, categories)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.categories.adapter = adapter
     }
 }
