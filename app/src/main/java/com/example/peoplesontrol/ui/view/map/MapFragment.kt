7162fced0a53c17e12cfeb10d53dcbe959a66202
Zellka <ilona.zelka@gmail.com>
2022-01-19 17:18:42 +0300

    Add current location

--- app/src/main/java/com/example/peoplesontrol/ui/view/map/MapFragment.kt
+++ app/src/main/java/com/example/peoplesontrol/ui/view/map/MapFragment.kt
@@ -1,11 +1,18 @@
 package com.example.peoplesontrol.ui.view.map
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import com.example.peoplesontrol.R
+import com.example.peoplesontrol.databinding.FragmentMapBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,33 +23,81 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
+    private var _binding: FragmentMapBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var currentLocation: Location
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_map, container, false)
+    ): View {
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this.requireActivity())
+        fetchLocation()
+    }
+
+    private fun fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this.requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this.requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+            ) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this.requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_CODE
+            )
+            return
+        }
+        val task = fusedLocationProviderClient.lastLocation
+        task.addOnSuccessListener {
+            if (it != null) {
+                currentLocation = it
+                val supportMapFragment = (childFragmentManager.findFragmentById(R.id.map) as
+                        SupportMapFragment?)!!
+                supportMapFragment.getMapAsync(this)
+            }
+        }
     }
 
     override fun onMapReady(p0: GoogleMap) {
-        p0.addMarker(
-            MarkerOptions()
-                .position(LatLng(47.9917, 37.7759))
-                .title("Donetsk")
-        )
-        p0.moveCamera(CameraUpdateFactory.newLatLng(LatLng(47.9917, 37.7759)))
-        p0.setOnMarkerClickListener(this)
+        val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
+        val markerOptions = MarkerOptions().position(latLng)
+        p0.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        p0.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        p0.addMarker(markerOptions)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                fetchLocation()
+            }
+        }
     }
 
     override fun onMarkerClick(p0: Marker): Boolean {
         val dialogAppealFragment = DialogAppealFragment()
         dialogAppealFragment.show(childFragmentManager, "MAP")
         return true
+    }
+
+    companion object {
+        private const val PERMISSION_CODE = 101
     }
 }
